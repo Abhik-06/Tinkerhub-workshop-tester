980e8b4d9c272e225ac5229463314ed63e4b25d9
Abhik-06 <abhik859070@gmil.com>
2024-10-15 18:31:37 +0530

    End of second day

---
+++ script.js
@@ -0,0 +1,38 @@
+
+    console.log("Hello guyscchchch")
+    let x = 69;
+    console.log(x)
+    x = 10;
+    console.log(x)
+    const y = 88;
+    console.log(y)
+    let z =1.92;
+    quote="hello guyse ambada kalla sunni kutta"
+    console.log(quote)
+    if(25>3){
+      console.log("YES")
+    }
+    else{
+      console.log("NEIN")
+    }
+    let count = 0;
+    let count2 = 0;
+    count2 = 2;
+    function Myfunc(){
+              count = count+1;
+              console.log('The count is : ',count,'Thank you')
+              console.log(` Count is : ${count}`)
+              console.log('haha hoho'); 
+                   }
+  
+
+function sub_caller(){
+    console.log("hello")
+
+    if(document.getElementById("Subber").innerText==="Subscribe");{
+        console.log("hey")
+        document.getElementById("Subber").innerText="Subscribed";
+        document.getElementById("Subber").classList.remove(".sub_button");
+        document.getElementById("Subber").classList.add("sub_button2"); }
+    
+  }
